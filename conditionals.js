49e3b2d1c638e31f557609915f8ef6cfaf86cdab
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-10 19:13:36 +0600

    if eles equell aita-na

---
+++ conditionals.js
@@ -0,0 +1,36 @@
+var biscuitsPrice = 12;
+if(biscuitsPrice < 10){
+    console.log("I will eat it")
+}
+else{
+    console.log("Khali cha is better")
+}
+
+// double = dite hobe
+
+var biscuitsPrice = 12;
+if(biscuitsPrice == 10){
+    console.log("I will eat it")
+}
+else{
+    console.log("Khali cha is better")
+}
+
+// 12 er soman na hole
+// mane 12 howa jabena
+
+var biscuitsPrice = 10;
+if(biscuitsPrice != 12){
+    console.log("I will eat it")
+}
+else{
+    console.log("Khali cha is better")
+}
+
+var biscuitsPrice = 12;
+if(biscuitsPrice != 12){
+    console.log("I will eat it")
+}
+else{
+    console.log("Khali cha is better")
+}
