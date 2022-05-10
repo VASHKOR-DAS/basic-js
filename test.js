9ead79b93296f809007a74cb221fa0180a63c3a5
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-10 15:49:11 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,37 @@
+console.log('Testing Node');
+var name = 12;
+console.log('name')
+var seenAfter = 21;
+console.log(typeof seenAfter)
+
+var Joya = ("Joya I Love YoU")
+console.log(Joya.toUpperCase());
+
+// find string
+
+var Joya = ("Joya I Love YoU")
+console.log(Joya.indexOf ("L"));
+
+// jog kora
+
+var number1 = 25; 
+// [intezer bole]
+var number2 = 15.5; 
+// [float bole]
+console.log(number1+number2);
+
+// jodi number+string hoy
+
+var number1 = 25;
+var number2 = "15.5";
+console.log(number1+number2);
+
+// string k number a convert korar niom
+
+var number1 = 25;
+var number2 = "15.5";
+number2 = parseFloat(number2)
+console.log(number1+number2);
+
+
+
