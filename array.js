8403e6ea1cf2c269ffc31471655c6f13a340d732
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-11 20:30:19 +0600

    edited

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-// var er akathik value likhte hole tar majhe comma dite hoy
+// var er akadhik value likhte hole tar majhe comma dite hoy
 
 var friendsAge = [15, 17, 14, 16];
 console.log(friendsAge);
@@ -15,14 +15,14 @@ var friendsAge = [15, 17, 14, 16];
 friendsAge[1] = 50;
 console.log(friendsAge);
 
-// search by name
+// position set kora, nam onujayi
 
 var friendsAge = [15, 17, 14, 16];
 sonaliAge = friendsAge[3];
 console.log(sonaliAge);
 
 // pisition janar jnno
-// like 14 er position jana nai
+// like "14 er position jana nai"
 
 var friendsAge = [15, 17, 14, 16];
 var position = friendsAge.indexOf(14);
@@ -34,13 +34,13 @@ var friendsAge = [15, 17, 14, 16];
 var position = friendsAge.indexOf(18);
 console.log(position);
 
-// Array Push array eer majhe kono element push korte chaile array .push
+// array er var er majhe kono element last a add korte chaile (.push)
 
 var friendsAge = [15, 17, 14, 16];
 friendsAge.push(18);
 console.log(friendsAge);
 
-// akadhik Element add korte chaile
+// last a akadhik Element add korte chaile
 var friendsAge = [15, 17, 14, 16];
 friendsAge.push(18);
 friendsAge.push(19);
@@ -50,14 +50,14 @@ console.log(friendsAge);
 var friendsAge = [15, 17, 14, 16];
 console.log(friendsAge.length)
 
-// push korar por
+// push korar por length
 
 var friendsAge = [15, 17, 14, 16];
 friendsAge.push(18);
 friendsAge.push(19);
 console.log(friendsAge.length);
 
-// last er kono Element k ber korte hole .pop
+// last er kono Element k ber korte hole (.pop)
 
 var friendsAge = [15, 17, 14, 16];
 console.log(friendsAge);
