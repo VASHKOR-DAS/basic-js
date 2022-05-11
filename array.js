6d7ba3694fa027477de761d95d2f4be44f82c9bf
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-11 20:07:17 +0600

    array indexOf

---
+++ array.js
@@ -0,0 +1,35 @@
+// var er akathik value likhte hole tar majhe comma dite hoy
+
+var friendsAge = [15, 17, 14, 16]
+console.log(friendsAge)
+
+// array er 1st possition 0
+//  position search korte hole [] dite hoy
+
+var friendsAge = [15, 17, 14, 16]
+console.log(friendsAge[1])
+
+// var er value change korte hole
+
+var friendsAge = [15, 17, 14, 16]
+friendsAge[1] = 50
+console.log(friendsAge)
+
+// search by name
+
+var friendsAge = [15, 17, 14, 16]
+sonaliAge = friendsAge[3]
+console.log(sonaliAge)
+
+// pisition janar jnno
+// like 14 er position jana nai
+
+var friendsAge = [15, 17, 14, 16]
+var position = friendsAge.indexOf(14)
+console.log(position)
+
+// jodi kono element na thake array er majhe tobe -1 result dibe
+
+var friendsAge = [15, 17, 14, 16]
+var position = friendsAge.indexOf(18)
+console.log(position)
