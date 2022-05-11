2c5753bbedf8937cfa68c3c9b71aa95c9e9e73fc
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-11 20:18:20 +0600

    .push .length added

--- array.js
+++ array.js
@@ -1,35 +1,59 @@
 // var er akathik value likhte hole tar majhe comma dite hoy
 
-var friendsAge = [15, 17, 14, 16]
-console.log(friendsAge)
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge);
 
 // array er 1st possition 0
 //  position search korte hole [] dite hoy
 
-var friendsAge = [15, 17, 14, 16]
-console.log(friendsAge[1])
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge[1]);
 
 // var er value change korte hole
 
-var friendsAge = [15, 17, 14, 16]
-friendsAge[1] = 50
-console.log(friendsAge)
+var friendsAge = [15, 17, 14, 16];
+friendsAge[1] = 50;
+console.log(friendsAge);
 
 // search by name
 
-var friendsAge = [15, 17, 14, 16]
-sonaliAge = friendsAge[3]
-console.log(sonaliAge)
+var friendsAge = [15, 17, 14, 16];
+sonaliAge = friendsAge[3];
+console.log(sonaliAge);
 
 // pisition janar jnno
 // like 14 er position jana nai
 
-var friendsAge = [15, 17, 14, 16]
-var position = friendsAge.indexOf(14)
-console.log(position)
+var friendsAge = [15, 17, 14, 16];
+var position = friendsAge.indexOf(14);
+console.log(position);
 
 // jodi kono element na thake array er majhe tobe -1 result dibe
 
-var friendsAge = [15, 17, 14, 16]
-var position = friendsAge.indexOf(18)
-console.log(position)
+var friendsAge = [15, 17, 14, 16];
+var position = friendsAge.indexOf(18);
+console.log(position);
+
+// Array Push array eer majhe kono element push korte chaile array .push
+
+var friendsAge = [15, 17, 14, 16];
+friendsAge.push(18);
+console.log(friendsAge);
+
+// akadhik Element add korte chaile
+var friendsAge = [15, 17, 14, 16];
+friendsAge.push(18);
+friendsAge.push(19);
+console.log(friendsAge);
+
+// array er length koto ta jante chaile
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge.length)
+
+// push korar por
+
+var friendsAge = [15, 17, 14, 16];
+friendsAge.push(18);
+friendsAge.push(19);
+console.log(friendsAge.length);
+
